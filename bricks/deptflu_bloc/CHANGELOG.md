2b0fdd2f8693e087d08897861fd1025e3597c480
Rahardiyan Eko Widiatmoko <rahardiyan@deptechdigital.com>
2024-10-10 14:24:07 +0700

    Adding option to create validation Cubit and State class

--- bricks/deptflu_bloc/CHANGELOG.md
+++ bricks/deptflu_bloc/CHANGELOG.md
@@ -1,3 +1,18 @@
-# 0.3.1
+# CHANGELOG
+
+## 0.1.1
+
+- Generating Bloc, Event, and State based on user's input
+
+## 0.2.1
+
+- Generating Cubit, Event, and State based on user's input
+- Adding `freezed` as immutable option
+
+## 0.3.1
 
 - Adding functionality to handle pagination in Bloc/Cubit class
+
+## 0.4.1
+
+- Adding option to create validation Cubit and State class
--- bricks/deptflu_bloc/hooks/pre_gen.dart
+++ bricks/deptflu_bloc/hooks/pre_gen.dart
@@ -3,14 +3,23 @@ import 'package:mason/mason.dart';
 void run(HookContext context) {
   final immutable = context.vars['immutable'];
   final type = context.vars['type'];
+  final validation = context.vars['validation'];
 
   final isUsingEquatable = immutable == 'equatable';
   final isUsingFreezed = immutable == 'freezed';
+
   final isUsingBloc = type == 'bloc';
   final isUsingCubit = type == 'cubit';
 
+  final isValidationNone = validation == 'none';
+  final isValidationFormz = validation == 'formz';
+
   context.vars['isUsingEquatable'] = isUsingEquatable;
   context.vars['isUsingFreezed'] = isUsingFreezed;
+
   context.vars['isUsingBloc'] = isUsingBloc;
   context.vars['isUsingCubit'] = isUsingCubit;
+
+  context.vars['isValidationNone'] = isValidationNone;
+  context.vars['isValidationFormz'] = isValidationFormz;
 }
